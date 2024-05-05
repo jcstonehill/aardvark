ec07ed7b502c941ce1daab4b8242df22bc1c2c45
Jake Stonehill <jacob.c.stonehill@gmail.com>
2024-05-04 22:45:02 -0400

    Added static conditions and more outputs to fc.

--- aardvark/components/flow_channel/flow_channel.py
+++ aardvark/components/flow_channel/flow_channel.py
@@ -1,5 +1,7 @@
 import aardvark as adv
 
+import numpy as np
+
 class FlowChannel1D(adv.ComponentBase):
 
 # https://cardinal.cels.anl.gov/theory/thm.html
@@ -8,13 +10,25 @@ class FlowChannel1D(adv.ComponentBase):
         "T0_in"     :   300,
         "P0_in"     :   101325,
         "m_dot"     :   0.0008,
-        "T_wall_z"  :   [0, 1],
+        "T_wall_z"  :   [0.5],
         "T_wall"    :   [300]
     }
 
     outputs = {
-        "Q_dot_z"   :   [0, 1],
-        "Q_dot"     :   [0]
+        "cell_z"    :   [0.5],
+        "Q_dot"     :   [0],
+        "ff_flow"   :   [None],
+        "Nu_flow"   :   [None],
+        "node_z"    :   [0, 1],
+        "Re_flow"   :   [None, None],
+        "T_flow"    :   [300, 300],
+        "P_flow"    :   [101325, 101325],
+        "rho_flow"  :   [None, None],
+        "v_flow"    :   [None, None],
+        "cp_flow"   :   [None, None],
+        "m_dot"     :   0.0008,
+        "T0_out"    :   300,
+        "P0_out"    :   101325
     }
 
     ic = {
@@ -22,7 +36,7 @@ class FlowChannel1D(adv.ComponentBase):
     }
 
     def __init__(self, N: int, A: float,  L: float, fluid: adv.FluidBase, 
-            nu_cor: adv.NuCorrelationBase, ff_cor: adv.FFCorrelationBase):
+            nu_cor: adv.NuCorrelationBase, ff_cor: adv.FFCorrelationBase, fp_tol = 1e-6, max_fp_iter = 100):
         """TODO fill this out.
         """
         self.N = N
@@ -31,6 +45,36 @@ class FlowChannel1D(adv.ComponentBase):
         self.fluid = fluid
         self.nu_cor = nu_cor
         self.ff_cor = ff_cor
+
+        self.fp_tol = fp_tol
+        self.max_fp_iter = max_fp_iter
+
+    def get_static_conditions(self, T0: float, P0: float, m_dot: float, A: float) -> tuple:
+        T = T0
+        P = P0
+
+        for _ in range(self.max_fp_iter):
+            rho = self.fluid.rho(T, P)
+            cp = self.fluid.cp(T, P)
+
+            v = m_dot / (rho*A)
+
+            T_prev = T
+            P_prev = P
+
+            T = T0 - v**2/(2*cp)
+            P = P0 - 0.5*rho*v**2
+
+            res = np.sqrt(((T-T_prev)/T)**2 + ((P-P_prev)/P)**2)
+
+            if(res < self.fp_tol):
+                break
+
+        return T, P, v
+
+
+
+        
 
     def solve(self, dt: int):
         """TODO fill this out.
@@ -42,4 +86,12 @@ class FlowChannel1D(adv.ComponentBase):
         T_wall_z = self.inputs["T_wall_z"]
         T_wall = self.inputs["T_wall"]
 
-        
+        T_in, P_in = self.get_static_conditions(T0_in, P0_in, m_dot, self.A)
+
+        T_flow = np.array(self.N+1)
+        P_flow = np.array(self.N+1)
+
+        T_flow[0] = T_in
+        P_flow[0] = P_in
+
+        for _ in range
--- aardvark/materials/fluids/fluid_base.py
+++ aardvark/materials/fluids/fluid_base.py
@@ -1,3 +1,10 @@
 from abc import ABC, abstractmethod
 class FluidBase(ABC):
-    pass
+    
+    @abstractmethod
+    def rho(self, T: float, P: float) -> float:
+        pass
+
+    @abstractmethod
+    def cp(self, T: float, P: float) -> float:
+        pass
