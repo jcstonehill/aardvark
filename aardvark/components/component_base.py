20476d3a6f79b81ad5be2a11198b01a5c41e0da3
Jake Stonehill <jacob.c.stonehill@gmail.com>
2024-05-17 13:43:38 -0400

    Reworked inputs, opt_inputs, and outputs.

--- aardvark/components/component_base.py
+++ aardvark/components/component_base.py
@@ -2,37 +2,50 @@ import aardvark.internal_api as adv
 
 from abc import ABC, abstractmethod
 
+import numpy as np
+
 class DataSetBase(ABC):
     pass
 
 class ComponentBase(ABC):
     
-    @property
-    @abstractmethod
-    def inputs(self):
-        pass
+    def __init__(self):
+        self.inputs = DataSetBase()
+        self.opt_inputs = DataSetBase()
+        self.outputs = DataSetBase()
 
-    @property
     @abstractmethod
-    def outputs(self):
-        pass
-    
-    @abstractmethod
-    def solve(self, dt: int):
-        """Solve for the component's solution variables at next timestep.
-
-        Take initial conditions (self.ic), and the inputs (self.inputs), and
-        solve for the outputs (self.outputs)
-
-        Parameters
-        ----------
-        dt : int
-            Time step (s)
+    def solve_steady_state(self):
+        """TODO
         """
         pass
 
     def check(self):
-        for property, value in vars(self.inputs).items():
+
+        inputs = vars(self.inputs)
+
+        for property, value in inputs.items():
             if(value is None):
                 # TODO rewrite error message.
                 raise Exception("Error input not connected")
+            
+            if(type(value) is float):
+                inputs[property] = adv.FloatVar(value)
+
+            if(type(value) is int):
+                inputs[property] = adv.FloatVar(float(value))
+
+            if(type(value) is list):
+                inputs[property] = adv.FloatArrayVar(np.array(value))
+
+            if(type(value) is np.ndarray):
+                inputs[property] = adv.FloatArrayVar(value)
+
+        opt_inputs = vars(self.opt_inputs)
+
+        for property, value in opt_inputs.items():
+            if(value is None):
+                opt_inputs[property] = adv.NoneVar()
+
+            if(type(value) is list):
+                opt_inputs[property] = adv.FloatArrayVar(np.array(value))
